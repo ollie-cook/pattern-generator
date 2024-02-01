a38de87a7ba2519d26693bcb03e2cf77f506655c
Oliver Cook <olliewatford@gmail.com>
2024-02-01 09:43:53 +0000

    You can now export the svg

--- app/components/Generator/Generator.tsx
+++ app/components/Generator/Generator.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react"
+import { useState, useRef } from "react"
 import SVGFrame from "./SVGFrame"
 import Controls from "./Controls"
 
@@ -10,10 +10,27 @@ export default function Generator() {
   const [bgColour, setBgColour] = useState('#53C18A');
   const [shapeColour, setShapeColour] = useState('#194D33');
 
+  const svgRef = useRef(null);
+
   const height = 100*9/16;
 
+  const exportSVG = () => {
+    if (svgRef.current == null) {
+      console.log("svgRef is null")
+      return;
+    }
+    const serializer = new XMLSerializer();
+    const svgStr = serializer.serializeToString(svgRef.current);
+    const blob = new Blob([svgStr], {type: "image/svg+xml"});
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'download.svg';
+    link.click();
+  };
+
   return (
-    <div className="min-h-screen flex flex-col justify-center">
+    <div className="min-h-screen flex flex-col justify-center items-center">
       <Controls 
         setShapeAmount={(value: number) => setShapeAmount(value)} 
         setShapeSize={(value: number) => setShapeSize(value)}
@@ -23,8 +40,14 @@ export default function Generator() {
         setShapeColour={(value: string) => setShapeColour(value)}
       />
       <div className="w-[50rem] aspect-[16/9] border border-black">
-        <SVGFrame height={height} shapeAmount={shapeAmount} shapeHeight={shapeSize} bgColour={bgColour} shapeColour={shapeColour} />
+        <SVGFrame ref={svgRef} height={height} shapeAmount={shapeAmount} shapeHeight={shapeSize} bgColour={bgColour} shapeColour={shapeColour} />
       </div>
+      <button 
+        className="p-2 bg-blue-500 text-white font-bold rounded-md mt-2 hover:bg-blue-600"
+        onClick = {exportSVG}
+      >
+        Export
+      </button>
     </div>
   )
 }
--- app/components/Generator/SVGFrame/SVGFrame.tsx
+++ app/components/Generator/SVGFrame/SVGFrame.tsx
@@ -1,10 +1,40 @@
+import { forwardRef } from 'react';
+
 interface SVGFrameProps {
   height: number;
   shapeAmount: number;
   shapeHeight: number;
   bgColour: string;
   shapeColour: string;
+  ref: any;
 }
+
+const SVGFrame = forwardRef<SVGSVGElement, SVGFrameProps>((props, ref) => {
+  let coords: number[][] = []; 
+
+  for (let i = 0; i < props.shapeAmount; i++) {
+    let x = Math.random()*(100-props.shapeHeight);
+    let y = Math.random()*(props.height-props.shapeHeight);
+    coords.push([x, y]);
+  }
+
+  return (
+    <svg ref={ref} viewBox={`0 0 100 ${props.height}`}>
+      <rect x="0" y="0" width="100" height={props.height} fill={props.bgColour}/>
+      {
+        Array(props.shapeAmount).fill(0).map((_, i) => {
+          let x = coords[i][0];
+          let y = coords[i][1];
+          return <rect key={i} x={x} y={y} width={props.shapeHeight} height={props.shapeHeight} fill={props.shapeColour}/>
+        })
+      }
+    </svg>
+  )
+});
+
+export default SVGFrame;
+
+/*
 
 export default function SVGFrame(props: SVGFrameProps) {
   let coords: number[][] = []; 
@@ -16,7 +46,7 @@ export default function SVGFrame(props: SVGFrameProps) {
   }
 
   return (
-    <svg viewBox={`0 0 100 ${props.height}`}>
+    <svg ref={props.ref} viewBox={`0 0 100 ${props.height}`}>
       <rect x="0" y="0" width="100" height={props.height} fill={props.bgColour}/>
       {
         Array(props.shapeAmount).fill(0).map((_, i) => {
@@ -28,3 +58,4 @@ export default function SVGFrame(props: SVGFrameProps) {
     </svg>
   )
 }
+*/
